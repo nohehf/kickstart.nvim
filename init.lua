e1f632488ceb54cf483623e6136655c3a9c8cadd
nohehf <nohe.hinniger.foray@gmail.com>
2024-11-29 18:30:06 +0100

    theme

--- init.lua
+++ init.lua
@@ -618,7 +618,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-
+        -- gleam = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -832,11 +832,18 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -878,6 +885,8 @@ require('lazy').setup({
       statusline.section_location = function()
         return '%2l:%-2v'
       end
+
+      require('mini.comment').setup()
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
